02db3c4855c5530a3a135997515c1b32e45caa94
Tshepiso28 <tshepisostudent@gmail.com>
2025-02-04 12:25:23 +0200

    Type casting

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ name = "Tshepiso"
 print(name)
 
 first_name = input("What is your first name?: ")
-age = input("What is your age?: ")
+age = int(input("What is your age?: "))
 
 print("Hello " + first_name + " you are " + age + " years old!")
